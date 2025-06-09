129c897d0158af29a9b222e2db1081766f0aac5a
SamuelZambrana <samu-1994@hotmail.es>
2025-06-09 18:34:04 +0200

    ADD: 8 class

---
+++ 08.octava-clase/pilotosDakarListaActualizada/src/components/InfoPilotoComponent.jsx
@@ -0,0 +1,65 @@
+const InfoPilotoComponent = ({
+  piloto,
+  deletePiloto,
+  selectPiloto,
+  backToList,
+  showInfo,
+}) => {
+  return (
+    <div className="card-piloto">
+      <div className="info-piloto">
+        {showInfo && (
+        <p>
+          <span className="title-prop">Id: </span>
+          <span className="title-data">{piloto.id}</span>
+        </p>
+        )
+      }
+        <p>
+          <span className="title-prop">Nombre: </span>
+          <span className="title-data">{piloto.nombre}</span>
+        </p>
+        <p>
+          <span className="title-prop">Pais de origen: </span>
+          <span className="title-data">{piloto.paisDeOrigen}</span>
+        </p>
+        {showInfo ? (
+        <p>
+          <span className="title-prop">Fecha de nacimiento: </span>
+          <span className="title-data">{piloto.fechaDeNacimiento}</span>
+        </p>
+        ):(null)}
+        <p>
+          <span className="title-prop">Vehículo: </span>
+          <span className="title-data">{piloto.vehiculo}</span>
+        </p>
+      </div>
+      <div className="utils-piloto">
+        {!showInfo ? (
+          <>
+            <button
+              className="btn-delete"
+              onClick={() => deletePiloto(piloto.id)}
+            >
+              Eliminar
+            </button>
+            <button
+              className="btn-select"
+              onClick={() => selectPiloto(piloto.id)}
+            >
+              Seleccionar
+            </button>
+          </>
+        ) : (
+          <>
+            <button className="btn-return" onClick={() => backToList()}>
+              Volver al listado
+            </button>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default InfoPilotoComponent;
---
+++ 08.octava-clase/pilotosDakarListaActualizada/src/pages/ListaPilotosPage.jsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from "react";
+import {
+  deletePilotoFetch,
+  getPilotoById,
+  getPilotos,
+} from "../services/pilotosFetch";
+import InfoPilotoComponent from "../components/InfoPilotoComponent";
+
+const ListaPilotosPage = () => {
+  const [listaPilotos, setListaPilotos] = useState(undefined);
+  const [pilotoSelected, setPilotoSelected] = useState(undefined);
+
+  const loadPilotos = () => {
+    console.log("Aqui llego");
+    const aux = getPilotos();
+    setListaPilotos(aux);
+  };
+
+  const deletePiloto = (idPiloto) => {
+    deletePilotoFetch(idPiloto);
+    loadPilotos();
+  };
+
+  const loadInfoPiloto = (idPiloto) => {
+    setPilotoSelected(getPilotoById(idPiloto));
+  };
+
+  const backToList = () => {
+    setPilotoSelected(undefined);
+  };
+
+  useEffect(() => {
+    loadPilotos();
+  }, []);
+
+  return (
+    <div className="full-container">
+      {!listaPilotos ? (
+        <div>Cargando pilotos...</div>
+      ) : (
+        <>
+          {pilotoSelected ? (
+            <InfoPilotoComponent
+              piloto={pilotoSelected}
+              backToList={backToList}
+              showInfo
+            />
+          ) : (
+            <>
+              <h1>Lista de pilotos</h1>
+              {listaPilotos.map((p, idx) => (
+                <>
+                  <InfoPilotoComponent
+                    key={idx}
+                    piloto={p}
+                    deletePiloto={deletePiloto}
+                    selectPiloto={loadInfoPiloto}
+                  />
+                </>
+              ))}
+            </>
+          )}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default ListaPilotosPage;
